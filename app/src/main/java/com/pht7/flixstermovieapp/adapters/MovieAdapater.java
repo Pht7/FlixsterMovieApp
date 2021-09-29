03e5dd2c409be7b8adaad3ac067d9a67588a89c3
peter <pht7@njit.edu>
2021-09-29 13:21:02 -0400

    Add item

--- app/src/main/java/com/pht7/flixstermovieapp/adapters/MovieAdapater.java
+++ app/src/main/java/com/pht7/flixstermovieapp/adapters/MovieAdapater.java
@@ -1,19 +1,25 @@
 package com.pht7.flixstermovieapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.pht7.flixstermovieapp.DetailActivity;
 import com.pht7.flixstermovieapp.R;
 import com.pht7.flixstermovieapp.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -48,6 +54,7 @@ public class MovieAdapater extends RecyclerView.Adapter<MovieAdapater.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -59,12 +66,28 @@ public class MovieAdapater extends RecyclerView.Adapter<MovieAdapater.ViewHolder
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+
+
+            tvTitle.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+
+                }
+            });
+
+
 
         }
     }
